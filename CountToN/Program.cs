74c67a97dff8ec7028d8247de1e6f319e9392952
GET Academy <terje@getacademy.no>
2021-11-23 09:32:44 +0100

    Initial commit

---
+++ CountToN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CountToN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new SimpleCountToN(5);
+            while (!counter.Count())
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ CountToN/SimpleCountToN.cs
@@ -0,0 +1,19 @@
+﻿namespace CountToN
+{
+    class SimpleCountToN
+    {
+        private readonly int _n;
+        private int _count;
+
+        public SimpleCountToN(int n)
+        {
+            _n = n;
+        }
+
+        public bool Count()
+        {
+            _count++;
+            return _count == _n;
+        }
+    }
+}
